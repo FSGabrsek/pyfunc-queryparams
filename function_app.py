3b4c5871ee22dde960404d3d668970af717dcd86
FSGabrsek <f.gabrsek@gmail.com>
2024-03-20 09:40:57 +0100

    added urllib example

--- function_app.py
+++ function_app.py
@@ -2,6 +2,7 @@ import azure.functions as func
 import logging
 
 import json
+from urllib.parse import urlparse, parse_qs
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -21,13 +22,41 @@ Note: Trailing comma results in additional 'empty' value (can be filtered out)
 """
 @app.route(route="ex1/articles",
            methods=('GET',))
-def http_get_article(req: func.HttpRequest) -> func.HttpResponse: 
+def http_get_articles_split(req: func.HttpRequest) -> func.HttpResponse: 
     # Get 'id' param or default to empty string
     article_param = req.params.get("id", "")
     logging.info(f"article_id query param \"{article_param}\", {type(article_param)}")
 
     # split string on comma
     articles = article_param.split(",")
+    logging.info(f"articles list {articles}, {type(articles)}")
+
+    # remove duplicates
+    articles = list(set(articles))
+
+    return func.HttpResponse(json.dumps({
+        "articles": articles
+    }))
+
+"""
+# Example 2: parse with urllib
+use urllib to parse url's and query strings
+
+concetanated parameters
+/articles?id=151.010.001?id=151.010.002?id=151.010.003
+
+Note: Requires 'urllib' package"
+pip install urllib
+"""
+@app.route(route="ex2/articles",
+           methods=('GET',))
+def http_get_articles_urllib(req: func.HttpRequest) -> func.HttpResponse: 
+    # use urllib to parse query params from url
+    query_params = parse_qs(urlparse(req.url).query)
+    logging.info(f"quary parameters {query_params}, {type(query_params)}")
+
+    # get article id's from quary params or default to empty list
+    articles = query_params.get("id", [])
     logging.info(f"articles list {articles}, {type(articles)}")
 
     # remove duplicates
